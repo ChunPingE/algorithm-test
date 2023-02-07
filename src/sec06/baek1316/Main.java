cf8acee83b8a29f5de6919c2a2e06eda58c170f9
parkchunsu <pcc0105@gmail.com>
2023-02-07 17:17:28 +0900

    2023.02.07

---
+++ src/sec06/baek1316/Main.java
@@ -0,0 +1,20 @@
+package sec06.baek1316;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+			
+		String str = sc.next();
+		
+
+
+		
+	}
+
+}
