188d96926622574e70c37ff5bc7803f3da883744
parkchunsu <pcc0105@gmail.com>
2023-10-24 15:46:19 +0900

    231024

---
+++ src/SQLTEST/MOD.java
@@ -0,0 +1,10 @@
+package SQLTEST;
+
+public class MOD {
+    public static void main(String[] args) {
+        System.out.println(15 % 4);
+        System.out.println(15 % -4);
+        System.out.println(-15 % 4);
+        System.out.println(-15 % -4);
+    }
+}
