427b5d15cc9cf6d2fd1ea94436993553be8f96a5
parkchunsu <pcc0105@gmail.com>
2023-12-04 10:10:00 +0900

    4

---
+++ src/programmers/d20231204/배열만들기4.java
@@ -0,0 +1,30 @@
+package programmers.d20231204;
+
+import java.util.Arrays;
+
+public class 배열만들기4 {
+	public int[] solution(int[] arr) {
+		int[] stk = {};
+		int index = 0;
+		for (int i = 0; i < arr.length;) {
+			if (stk.length == 0) {
+				stk = new int[1];
+				stk[index++] = arr[i];
+				i++;
+			} else {
+				if (stk[stk.length - 1] < arr[i]) {
+					stk = Arrays.copyOf(stk, stk.length + 1);
+					stk[stk.length - 1] = arr[i];
+					i++;
+				} else {
+					stk = Arrays.copyOf(stk, stk.length - 1);
+					index--;
+					if (index < 0) {
+						index = 0;
+					}
+				}
+			}
+		}
+		return stk;
+	}
+}
